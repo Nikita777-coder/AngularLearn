84aa3050c7ac5459d7144743b87af2c612b44561
Никита Ягло <jaglo@itgeo.local>
2024-10-31 17:10:40 +0300

    make app form

--- tdf/src/app/app.component.ts
+++ tdf/src/app/app.component.ts
@@ -1,13 +1,20 @@
+import { JsonPipe, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, FormsModule, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'tdf';
+  protected topics = [
+    'Backend',
+    'Frontend',
+    'AI'
+  ]
 }
