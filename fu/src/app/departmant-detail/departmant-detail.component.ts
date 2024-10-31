00a999db0acc777e149deb07aa5589eb04998100
Никита Ягло <jaglo@itgeo.local>
2024-10-31 16:00:40 +0300

    make department details

--- fu/src/app/departmant-detail/departmant-detail.component.ts
+++ fu/src/app/departmant-detail/departmant-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-departmant-detail',
@@ -7,6 +8,48 @@ import { Component } from '@angular/core';
   templateUrl: './departmant-detail.component.html',
   styleUrl: './departmant-detail.component.css'
 })
-export class DepartmantDetailComponent {
+export class DepartmantDetailComponent implements OnInit {
+  protected id = 0;
 
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+    let param = this.activatedRoute.snapshot.paramMap.get("id");
+    let id = parseInt(param ? param : "");
+
+    if (id > 5) {
+        this.router.navigate(['/fupage']);
+      }
+
+    this.id = id;
+  }
+
+  private getNavigationId(previous: boolean): void {
+    if (this.id == 1 && previous) {
+      this.id = 5;
+      return;
+    }
+
+    if (this.id == 5 && !previous) {
+      this.id = 1;
+      return;
+    }
+
+    let one = previous ? -1 : 1;
+    this.id += one;
+  }
+
+  private navigate() {
+    this.router.navigate(['/departmants', this.id]);
+  }
+
+  goPrevious(): void {
+    this.getNavigationId(true);
+    this.navigate()
+  }
+
+  goNext(): void {
+    this.getNavigationId(false);
+    this.navigate()
+  }
 }
