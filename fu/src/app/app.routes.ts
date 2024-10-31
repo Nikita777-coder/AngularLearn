a0f7100d12d82afb327e7184104e86f1c33a28cc
Никита Ягло <jaglo@itgeo.local>
2024-10-31 14:02:04 +0300

    make department list

--- fu/src/app/app.routes.ts
+++ fu/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { DepartmantOverviewComponent } from './departmant-overview/departmant-overview.component';
+import { DepartmantDetailComponent } from './departmant-detail/departmant-detail.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DepartmantListComponent } from './departmant-list/departmant-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/departmants', pathMatch: 'full' },
+  { path: 'departmants', component: DepartmantListComponent },
+  { path: 'departmants/:id', component: DepartmantDetailComponent },
+  { path: 'employees', component: EmployeeListComponent },
+  { path: '**', component: PageNotFoundComponent}
+];
--- fu/src/app/departmant-list/departmant-list.component.ts
+++ fu/src/app/departmant-list/departmant-list.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-departmant-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './departmant-list.component.html',
   styleUrl: './departmant-list.component.css'
 })
-export class DepartmantListComponent {
+export class DepartmantListComponent implements OnInit {
+  public selectId = 0;
 
+  public departmants = [
+    {"id": 1, "name": "angular"},
+    {"id": 2, "name": "Node"},
+    {"id": 3, "name": "MongoDB"},
+    {"id": 4, "name": "Ruby"},
+    {"id": 5, "name": "Bootstrap"}
+  ]
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let p = params.get('id');
+      let id = parseInt(p ? p : "");
+
+      this.selectId = id;
+    
+    } );
+  }
+
+  onSelect(department: { id: number; }) {
+    //this.router.navigate(['/departments', department.id]);
+     this.router.navigate([department.id], { relativeTo: this.route });
+  }
+
+  isSelected(department: { id: number; }) { return department.id === this.selectId; }
 }
